8102943dd5b61024ea8089ef3c454d54b8c76664
Phong Tran <phong.tran1@tiki.vn>
2022-11-26 15:14:36 +0700

    workspace

---
+++ README.md
@@ -0,0 +1,4 @@
+Front-end Experimental
+
+1. Webpack + ESBuild Loader
+2. ESBuild Bundler
---
+++ esbuild-bundler/src/App.tsx
@@ -0,0 +1,8 @@
+import React, { FC, ReactElement } from 'react';
+import Pages from './pages';
+
+const App: FC = (): ReactElement => {
+  return <Pages />;
+};
+
+export default App;
